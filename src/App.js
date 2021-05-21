2943d3c6e764f4eec6c05ffd63a0218c0861d8cc
Matt Hale <mhale@factharbor.com>
2021-05-21 09:55:36 -0500

    change bio import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import Project from "./pages/Project/index";
-import Bio from "./pages/Bio/index";
+import Bio from "./pages/bio/index";
 import "./App.css";
 
 function App() {
